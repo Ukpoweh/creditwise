ea06e375332792b5697eceed8a213cae1ce8e6c5
Ukpoweh <ukpowehgift@gmail.com>
2024-05-30 12:21:10 +0000

    model integration

--- app.py
+++ app.py
@@ -6,57 +6,63 @@ import pandas as pd
 import numpy as np
 import pickle
 
+from datetime import datetime
+import time
+
+
 model = pickle.load(open('model.pkl', 'rb'))
 scaler = pickle.load(open('scaler.pkl', 'rb'))
 encoder = pickle.load(open('encoder.pkl', 'rb'))
 
+data = pd.read_csv("data.csv")
+
+gender_opt = list(data["CODE_GENDER"].unique())
+car_opt = list(data["FLAG_OWN_CAR"].unique())
+realty_opt = list(data["FLAG_OWN_REALTY"].unique())
+income = list(data["NAME_INCOME_TYPE"].unique())
+occupation = list(data["OCCUPATION_TYPE"].unique())
+education = list(data["NAME_EDUCATION_TYPE"].unique())
+family = list(data["NAME_FAMILY_STATUS"].unique())
+housing = list(data["NAME_HOUSING_TYPE"].unique())
+
 def main():
     st.title("CreditWise")
-    st.subheader("A Credit Approval Prediction System")
+    st.subheader("A Credit Card Approval Prediction System")
 
     #input prompts
+    st.write('----')
     col1, col2 = st.columns([1,1])
     with col1:
-        gender = st.radio("Select your gender", ['M', 'F'])
+        gender = st.radio("Select your gender", gender_opt)
     with col2:
         annual_income = st.number_input("Enter your annual income")
 
     col3, col4 = st.columns([1,1])
     with col3:
-        own_car = st.radio("Do you have a car", ["Y", "N"])
+        own_car = st.radio("Do you have a car", car_opt)
     with col4:
-        own_realty = st.radio("Do you have a property", ["Y", "N"])
+        own_realty = st.radio("Do you have a property", realty_opt)
     
     col5, col6 = st.columns([1,1])
     with col5:
-        income=['Working', 'Commercial associate', 'State servant', 'Student',
-       'Pensioner']
         income_type = st.selectbox("Select your type of income", income)
     with col6:
-        occupation=['Security staff', 'Sales staff', 'Accountants', 'Laborers',
-       'Managers', 'Drivers', 'Core staff', 'High skill tech staff',
-       'Cleaning staff', 'Private service staff', 'Cooking staff',
-       'Low-skill Laborers', 'Medicine staff', 'Secretaries',
-       'Waiters/barmen staff', 'HR staff', 'Realty agents', 'IT staff']
         occupation_type = st.selectbox("Select your type of occupation", occupation)
     
     col7, col8 = st.columns([1,1])
     with col7:
-        education=['Secondary / secondary special', 'Higher education',
-       'Incomplete higher', 'Lower secondary', 'Academic degree']``````````````
         education_level = st.selectbox("Select your level of education", education)
     with col6:
-        family=['Married', 'Single / not married', 'Civil marriage', 'Separated',
-       'Widow']
         family_status = st.selectbox("Select your family status", family)
 
     col9, col10 = st.columns([1,1])
     with col9:
-        housing=['House / apartment', 'Rented apartment', 'Municipal apartment',
-       'With parents', 'Co-op apartment', 'Office apartment']
         housing_type = st.selectbox("Select your type of housing", housing)
     with col10:
         start_employment_date = st.date_input("Enter the start date for your current emloyment")
+        #date_obj = datetime.strptime(start_employment_date, "%Y/%m/%d")
+        today = datetime.now().date()
+        days_difference = (start_employment_date-today).days
     
     col11, col12 = st.columns([1,1])
     with col11:
@@ -66,13 +72,22 @@ def main():
     
     col13, col14 = st.columns([1,1])
     with col13:
-        own_phone = st.radio("Do you have a phone", ["0", ""])
+        own_phone = st.radio("Do you have a phone", [0, 1])
     with col14:
-        own_email = st.radio("Do you have an email", ["0", "1"])
+        own_email = st.radio("Do you have an email", [0, 1])
+    st.write('----')
+
+    labeled_categories = np.array(encoder.fit_transform([gender, own_car, own_realty, income_type, education_level, family_status, housing_type, occupation_type])).reshape(1, -1)
+    numerical_features = np.array([annual_income, children_count, fam_count, own_phone, own_email, days_difference]).reshape(1,-1)
 
 
-    labeled_categories = np.array(encoder.transform([gender, own_car, own_realty, income_type, education_level, family_status, housing_type, occupation_type])).reshape(1, -1)
-    numerical_features = np.array([annual_income, children_count, fam_count, own_phone, own_email])
+    processed_categories = np.concatenate([labeled_categories, numerical_features], axis=1)
+    scaled_features = scaler.transform(processed_categories)
+
+    prediction = model.predict_proba(scaled_features)
+    approved_prob = np.round((prediction[:, 1] * 100), 2)[0]
+    if st.button('Predict your credit card approval probability'):
+        st.success(f"The probability that your credit card will be approved is {approved_prob}%")
 
 if __name__ == "__main__":
     main()
